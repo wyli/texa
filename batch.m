200dd098b226e99dda8e717f75344026bf96232e
W Li <wqli@me.com>
2013-08-11 21:13:42 +0100

    modified:   batch.m

--- batch.m
+++ batch.m
@@ -14,15 +14,15 @@
 %main('randomfeatures', 71, 13, 7, 200);
 %
 %main('randomfeatures', 11, 5, 3, 100);
-main('randomfeatures', 21, 5, 3, 100);
-main('randomfeatures', 31, 5, 3, 100);
-main('randomfeatures', 41, 5, 3, 100);
-main('randomfeatures', 51, 5, 3, 100);
-main('randomfeatures', 61, 5, 3, 100);
-main('randomfeatures', 71, 5, 3, 100);
+%main('randomfeatures', 21, 5, 3, 100);
+%main('randomfeatures', 31, 5, 3, 100);
+%main('randomfeatures', 41, 5, 3, 100);
+%main('randomfeatures', 51, 5, 3, 100);
+%main('randomfeatures', 61, 5, 3, 100);
+%main('randomfeatures', 71, 5, 3, 100);
 %
 %main('randomfeatures', 11, 3, 2, 27);
-main('randomfeatures', 21, 3, 2, 27);
+%main('randomfeatures', 21, 3, 2, 27);
 main('randomfeatures', 31, 3, 2, 27);
 main('randomfeatures', 41, 3, 2, 27);
 main('randomfeatures', 51, 3, 2, 27);
