a47ad1213033921717614b6ff14f117d090535e5
W Li <wqli@me.com>
2013-08-18 22:17:31 +0100

    modified:   batch.m

--- batch.m
+++ batch.m
@@ -6,21 +6,21 @@
 %main('randomfeatures_surs', 61, 9, 5, 200);
 %main('randomfeatures_surs', 71, 9, 5, 200);
 
-main('randomfeatures_surs', 21, 13, 7, 80);
-main('randomfeatures_surs', 31, 13, 7, 200);
-main('randomfeatures_surs', 41, 13, 7, 200);
-main('randomfeatures_surs', 51, 13, 7, 200);
-main('randomfeatures_surs', 61, 13, 7, 200);
-main('randomfeatures_surs', 71, 13, 7, 200);
-exit
-main('randomfeatures_surs', 11, 5, 3, 100);
-main('randomfeatures_surs', 21, 5, 3, 100);
-main('randomfeatures_surs', 31, 5, 3, 100);
-main('randomfeatures_surs', 41, 5, 3, 100);
-main('randomfeatures_surs', 51, 5, 3, 100);
-main('randomfeatures_surs', 61, 5, 3, 100);
-main('randomfeatures_surs', 71, 5, 3, 100);
-
+%main('randomfeatures_surs', 21, 13, 7, 80);
+%main('randomfeatures_surs', 31, 13, 7, 200);
+%main('randomfeatures_surs', 41, 13, 7, 200);
+%main('randomfeatures_surs', 51, 13, 7, 200);
+%main('randomfeatures_surs', 61, 13, 7, 200);
+%main('randomfeatures_surs', 71, 13, 7, 200);
+%exit
+%main('randomfeatures_surs', 11, 5, 3, 100);
+%main('randomfeatures_surs', 21, 5, 3, 100);
+%main('randomfeatures_surs', 31, 5, 3, 100);
+%main('randomfeatures_surs', 41, 5, 3, 100);
+%main('randomfeatures_surs', 51, 5, 3, 100);
+%main('randomfeatures_surs', 61, 5, 3, 100);
+%main('randomfeatures_surs', 71, 5, 3, 100);
+%exit
 main('randomfeatures_surs', 11, 3, 2, 27);
 main('randomfeatures_surs', 21, 3, 2, 27);
 main('randomfeatures_surs', 31, 3, 2, 27);
@@ -28,3 +28,4 @@ main('randomfeatures_surs', 41, 3, 2, 27);
 main('randomfeatures_surs', 51, 3, 2, 27);
 main('randomfeatures_surs', 61, 3, 2, 27);
 main('randomfeatures_surs', 71, 3, 2, 27);
+exit
